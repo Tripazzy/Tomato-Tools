ee2bfe8a6829f29d79a75b5b67da9dcfe012b700
lpasfiel <lpasfiel@as.edu.au>
2018-05-14 22:04:35 +1000

    Added a javadoc.

--- src/salsaboy/tomatotool/graphics/Direction.java
+++ src/salsaboy/tomatotool/graphics/Direction.java
@@ -1,5 +1,9 @@
 package salsaboy.tomatotool.graphics;
 
+/**
+ * A collection of all the directions.
+ * Pretty self-explanatory.
+ */
 public enum Direction {
     UP, DOWN, LEFT, RIGHT,
     TOPLEFT, TOPRIGHT, BOTTOMLEFT, BOTTOMRIGHT
