b55b136481551643c0c81d2eccd80931bfaa0823
lpasfiel <lpasfiel@as.edu.au>
2018-05-14 22:42:03 +1000

    Created the Pause class

---
+++ src/salsaboy/tomatotool/control/Pause.java
@@ -0,0 +1,23 @@
+package salsaboy.tomatotool.control;
+
+import javax.swing.*;
+
+/**
+ * A class used to pause for a certain amount of time
+ */
+public class Pause {
+    /**
+     * This is intended to be overridden.
+     * It runs when the timer is over.
+     */
+    private void onEnd() {}
+    private Timer timer;
+    
+    public Pause(int seconds) {
+        timer = new Timer(seconds, e -> onEnd());
+        timer.setRepeats(false);
+        timer.start();
+        
+        while (timer.isRunning()) {}
+    }
+}
