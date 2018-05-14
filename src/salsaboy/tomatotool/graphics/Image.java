3ec2b0c0fffdf5f3d5234f073080b9f2a6c9ad5f
lpasfiel <lpasfiel@as.edu.au>
2018-05-14 20:29:32 +1000

    Made the basics for the Image class.

--- src/salsaboy/tomatotool/graphics/Image.java
+++ src/salsaboy/tomatotool/graphics/Image.java
@@ -1,7 +1,18 @@
 package salsaboy.tomatotool.graphics;
 
-public class Image {
-    public Image() {
-    
+import javax.swing.*;
+import java.awt.*;
+
+/**
+ * Put a class that extends this in the package where your
+ * images are.
+ */
+public class Image extends JLabel {
+    public Image(String filename) {
+        this(filename, 0, 0);
+    }
+    public Image(String filename, int x, int y) {
+        super(new ImageIcon(Image.class.getResource(filename + ".png")));
+        setBounds(new Rectangle(new Point(x, y), getPreferredSize()));
     }
 }
