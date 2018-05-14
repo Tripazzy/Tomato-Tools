1d381772f21cfa44fc9ac3521af02267380cbade
lpasfiel <lpasfiel@as.edu.au>
2018-05-14 23:05:03 +1000

    Made most of the Window class

--- src/salsaboy/tomatotool/graphics/Window.java
+++ src/salsaboy/tomatotool/graphics/Window.java
@@ -1,4 +1,26 @@
 package salsaboy.tomatotool.graphics;
 
-public class Window {
+import javax.swing.*;
+import java.awt.*;
+
+public class Window extends JFrame {
+    @Override
+    public void paint(Graphics g) {
+        for (int i = 0; i < getComponentCount(); i++) {
+            getComponent(i).paint(g);
+        }
+    }
+    
+    public Window(int width, int height) {
+        this(width, height, null);
+    }
+    public Window(int width, int height, String title) {
+        super(title);
+        setLayout(null);
+        
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setSize(width, height);
+        setResizable(false);
+        setVisible(true);
+    }
 }
